f5ae45a6798c60ca7362031cf0d5a0759ded495d
Ryan Jarvis <ryan@shopboxretail.com>
2018-05-03 21:34:55 -0700

    PEP8 Whitespace.  Add file encoding.

--- doc/script/simple_stream.py
+++ doc/script/simple_stream.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=utf-8
 from __future__ import print_function
 
 """\
@@ -42,27 +43,27 @@ import serial
 import time
 
 # Open grbl serial port
-s = serial.Serial('/dev/tty.usbmodem1811',115200)
+s = serial.Serial('/dev/tty.usbmodem1811', 115200)
 
 # Open g-code file
-f = open('grbl.gcode','r');
+f = open('grbl.gcode', 'r')
 
 # Wake up grbl
 s.write("\r\n\r\n")
-time.sleep(2)   # Wait for grbl to initialize 
+time.sleep(2)  # Wait for grbl to initialize
 s.flushInput()  # Flush startup text in serial input
 
 # Stream g-code to grbl
 for line in f:
-    l = line.strip() # Strip all EOL characters for consistency
+    l = line.strip()  # Strip all EOL characters for consistency
     print('Sending: ' + l)
-    s.write(l + '\n') # Send g-code block to grbl
-    grbl_out = s.readline() # Wait for grbl response with carriage return
+    s.write(l + '\n')  # Send g-code block to grbl
+    grbl_out = s.readline()  # Wait for grbl response with carriage return
     print(' : ' + grbl_out.strip())
 
 # Wait here until grbl is finished to close serial port and file.
-raw_input("  Press <Enter> to exit and disable grbl.") 
+raw_input("  Press <Enter> to exit and disable grbl.")
 
 # Close file and serial port
 f.close()
-s.close()    
+s.close()
