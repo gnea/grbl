bb25d2f97e0e6afc9cbec70f532632b8f6803cc0
Sonny Jeon <chamnit@gmail.com>
2018-11-12 08:25:57 -0700

    Update grbl.h

--- grbl/grbl.h
+++ grbl/grbl.h
@@ -23,7 +23,7 @@
 
 // Grbl versioning system
 #define GRBL_VERSION "1.1g"
-#define GRBL_VERSION_BUILD "20180614"
+#define GRBL_VERSION_BUILD "20181112"
 
 // Define standard libraries used by Grbl.
 #include <avr/io.h>
