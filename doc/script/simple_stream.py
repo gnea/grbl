33ff68cc5e2ced199dd9577e012e6d797f8c57f7
tpltnt <1172976+tpltnt@users.noreply.github.com>
2022-10-09 11:57:52 +0200

    use 'with' for resource-allocating operation

--- doc/script/simple_stream.py
+++ doc/script/simple_stream.py
@@ -35,32 +35,30 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ---------------------
 """
-
-import serial
 import time
+import serial
+
 
 # Open grbl serial port
 s = serial.Serial('/dev/tty.usbmodem1811',115200)
 
 # Open g-code file
-f = open('grbl.gcode','r')
+with open('grbl.gcode','r') as f:
+    # Wake up grbl
+    s.write("\r\n\r\n")
+    time.sleep(2)   # Wait for grbl to initialize
+    s.flushInput()  # Flush startup text in serial input
 
-# Wake up grbl
-s.write("\r\n\r\n")
-time.sleep(2)   # Wait for grbl to initialize
-s.flushInput()  # Flush startup text in serial input
+    # Stream g-code to grbl
+    for line in f:
+        l = line.strip() # Strip all EOL characters for consistency
+        print(f"Sending {l}", end="")
+        s.write(l + '\n') # Send g-code block to grbl
+        grbl_out = s.readline() # Wait for grbl response with carriage return
+        print(' : ' + grbl_out.strip())
 
-# Stream g-code to grbl
-for line in f:
-    l = line.strip() # Strip all EOL characters for consistency
-    print(f"Sending {l}", end="")
-    s.write(l + '\n') # Send g-code block to grbl
-    grbl_out = s.readline() # Wait for grbl response with carriage return
-    print(' : ' + grbl_out.strip())
+    # Wait here until grbl is finished to close serial port and file.
+    input("  Press <Enter> to exit and disable grbl.")
 
-# Wait here until grbl is finished to close serial port and file.
-input("  Press <Enter> to exit and disable grbl.")
-
-# Close file and serial port
-f.close()
+# Close serial port
 s.close()
